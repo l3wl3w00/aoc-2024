6bcee58cbfc9a6dad0e48c053d07d1ffae90fdd0
gyorgydeakl <gyorgydeak.levente@gmail.com>
2024-12-08 18:42:31 +0100

    refactor day 8

--- Days/Day8.cs
+++ Days/Day8.cs
@@ -12,12 +12,11 @@ public class Day8 : IAocDay<int>
     public int SolvePart1(string path) => Solve(path, true);
     public int SolvePart2(string path) => Solve(path, false);
 
-    private int Solve(string path, bool checkTwiceDistance)
+    private static int Solve(string path, bool checkTwiceDistance)
     {
         var world = File.ReadLines(path)
             .SelectMany((line, row) => line.Select((c, col) => (Pos: new Vec2(col, row), Frequency: c)))
             .ToFrozenDictionary(c => c.Pos, c => c.Frequency);
-
         var worldByFrequencies = world
             .GroupBy(p => p.Value)
             .Where(g => g.Key != '.')
@@ -29,22 +28,22 @@ public class Day8 : IAocDay<int>
                     .ToList());
         return world.Keys.Count(pos => worldByFrequencies.Values.Any(f => IsAntinode(pos, f, checkTwiceDistance)));
     }
-    
-    private bool IsAntinode(Vec2 pos, List<Vec2> frequencyPositions, bool checkTwiceDistance) =>
+    private static bool IsAntinode(Vec2 pos, List<Vec2> frequencyPositions, bool checkTwiceDistance) =>
         frequencyPositions
-            .Any(freqPos => frequencyPositions
-                .Where(freqPos2 => freqPos2 != freqPos)
-                .Any(freqPos2 =>
-                {
-                    var pos1Diff = freqPos - pos;
-                    var pos2Diff = freqPos2 - pos;
-                    var enclosedArea = pos1Diff.X * pos2Diff.Y - pos1Diff.Y * pos2Diff.X;
-                    var arePointsOnTheSameLine = Math.Abs(enclosedArea) < 1e-7f;
+            .Join(frequencyPositions, _ => 0, _ => 0, (p1, p2) => (Pos1: p1, Pos2: p2))
+            .Where(frequencyPair => frequencyPair.Pos1 != frequencyPair.Pos2)
+            .Any(frequencyPair =>
+            {
+                var (freqPos1, freqPos2) = frequencyPair;
+                var pos1Diff = freqPos1 - pos;
+                var pos2Diff = freqPos2 - pos;
+                var enclosedArea = pos1Diff.X * pos2Diff.Y - pos1Diff.Y * pos2Diff.X;
+                var arePointsOnTheSameLine = Math.Abs(enclosedArea) < 1e-7f;
 
-                    var isTwiceDistance =
-                        Math.Abs(pos.DistanceTo(freqPos) - 2 * pos.DistanceTo(freqPos2)) < 0.0001 ||
-                        Math.Abs(2 * pos.DistanceTo(freqPos) - pos.DistanceTo(freqPos2)) < 0.0001;
+                var isTwiceDistance =
+                    Math.Abs(pos.DistanceTo(freqPos1) - 2 * pos.DistanceTo(freqPos2)) < 0.0001 ||
+                    Math.Abs(2 * pos.DistanceTo(freqPos1) - pos.DistanceTo(freqPos2)) < 0.0001;
 
-                    return arePointsOnTheSameLine && (!checkTwiceDistance || isTwiceDistance);
-                }));
+                return arePointsOnTheSameLine && (!checkTwiceDistance || isTwiceDistance);
+            });
 }
